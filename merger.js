b942a8d83d006c1fd5007d34f04b41bed955b363
Furkan Acar <furkan@acar.digital>
2020-09-12 01:55:47 +0300

    Created merger.

---
+++ merger.js
@@ -0,0 +1,5 @@
+const healthy = require("./extracted.corrupted.json");
+const corrupt = require("./extracted.corrupted.json");
+
+// TODO: Merge healthy onto corrupt data
+// TODO: Find missing authors by web scraping
